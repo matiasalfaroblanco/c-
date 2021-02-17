e2263b10aaeb3edd9750f172e38a3d9387fbffb8
matiasalfaroblanco <66249785+matiasalfaroblanco@users.noreply.github.com>
2021-02-17 10:08:12 +0100

    Int commit

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Modulo1.Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string type = Property.GetPropertyTypeById(1);
+            //Console.WriteLine(type);
+
+            //string description = "prueba";
+            //PostDescription postDescription = new PostDescription(description);
+
+            //int userId = 22;
+            //Console.WriteLine(User.CheckUserHasBeenDeleted(userId));
+        }
+    }
+}
